be695b38b56bc9fcf22b07c974a1a76653310eb2
msrocka <michael.srocka@gmail.com>
2017-07-11 14:35:55 +0200

    semicolons?

--- update-src/003_add_data_quality_system_64..55/script.py
+++ update-src/003_add_data_quality_system_64..55/script.py
@@ -1,7 +1,8 @@
 import org.openlca.util.DQSystems as DQSystems
 
+
 def callback(rs):
-	systemId = DQSystems.ecoinvent(db).getId();
+	systemId = DQSystems.ecoinvent(db).getId()
 	olca.updateSql("UPDATE tbl_processes SET f_exchange_dq_system = " + str(systemId) + " WHERE id IN (SELECT DISTINCT f_owner FROM tbl_exchanges WHERE dq_entry IS NOT NULL)")
 
 olca.querySql("SELECT count(id) FROM tbl_exchanges WHERE dq_entry IS NOT NULL", callback)
